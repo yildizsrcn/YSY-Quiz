0a36996a0f35f99de490db89cacd4ced9a472eaa
Yusuf Sercan YILDIZ <srcnyildiz@Stars-MacBook-Pro.local>
2024-01-18 19:19:15 +0000

    questions javascript has been created

---
+++ assets/js/questions.js
@@ -0,0 +1,15 @@
+const questions = [
+    {id: 1,
+      question: "What country has the highest life expectancy?",
+      answers: [["A", "France"], ["B", "United Kingdom"], ["C", "Hong Kong"], ["D", "U.S.A."]],
+      correctAnswer: "C"},
+    {id: 2,
+      question: "What year was the United Nations established?",
+      answers: [["A", "1945"], ["B", "1947"], ["C", "1943"], ["D", "1949"]],
+      correctAnswer: "A"},
+    {
+      id: 3,
+      question: "What country has won the most World Cups?",
+      answers: [["A", "Argentina"], ["B", "France"], ["C", "Brazil"], ["D", "England"]],
+      correctAnswer: "C"
+    }];
